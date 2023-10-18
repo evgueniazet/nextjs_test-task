bbc892b4c4714fc0c376422781eca995c52af2ca
evgueniazet <zelenkoea19@gmail.com>
2023-10-19 00:52:41 +0300

    feat: add pagination

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { Inter } from "next/font/google";
 import styles from "./Home.module.scss";
 import { createApi } from "unsplash-js";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import Button from "@mui/material/Button";
 import { SelectChangeEvent } from "@mui/material/Select";
 import CircularProgress from "@mui/material/CircularProgress";
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,19 +33,53 @@ type TPhoto = {
 };
 
 export default function Home() {
+  const router = useRouter();
   const [isLoading, setLoading] = useState(true);
   const [images, setImages] = useState<TPhoto[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
   const [dateSort, setDateSort] = useState<ESortDirection>(ESortDirection.ASC);
   const [likesSort, setLikesSort] = useState<ESortDirection>(
     ESortDirection.ASC
   );
+  const [page, setPage] = useState(1);
+  const [like, setLike] = useState(false);
+  const [likes, setLikes] = useState<string[]>([]);
+  const [activeUserEmail, setActiveUserEmail] = useState("");
 
   useEffect(() => {
-    if (selectedCategory) {
-      api.collections
-        .getPhotos({ collectionId: selectedCategory })
+    const isAuthorized = JSON.parse(
+      localStorage.getItem("authorized") || "false"
+    );
+
+    if (!isAuthorized) {
+      router.push(`/`);
+    } else {
+      const activeEmail = localStorage.getItem("user");
+      if (activeEmail) {
+        const favoritesImagesIds = localStorage.getItem(activeEmail);
+        const favoritesImagesIdsParsed = favoritesImagesIds
+          ? JSON.parse(favoritesImagesIds)
+          : [];
+
+        setActiveUserEmail(activeEmail);
+        setLikes(favoritesImagesIdsParsed);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    setPage(Number(router.query.page) || 1);
+    setSelectedCategory((router.query.category as string) || "all");
+  }, [router.query]);
+
+  useEffect(() => {
+    setLoading(true);
+    if (selectedCategory === "all") {
+      api.search
+        .getPhotos({ query: "all", orientation: "landscape", page })
         .then((result) => {
+          console.log("result", result);
+
           const photos: TPhoto[] =
             result.response?.results.map((item) => ({
               alt_description: item.alt_description || "",
@@ -55,14 +91,16 @@ export default function Home() {
             })) || [];
 
           setImages(photos);
+          setLoading(false);
         })
         .catch(() => {
           console.log("something went wrong!");
         });
     } else {
-      api.search
-        .getPhotos({ query: "all", orientation: "landscape" })
+      api.collections
+        .getPhotos({ collectionId: selectedCategory, page })
         .then((result) => {
+          console.log("result", result);
           const photos: TPhoto[] =
             result.response?.results.map((item) => ({
               alt_description: item.alt_description || "",
@@ -73,15 +111,14 @@ export default function Home() {
               url: item.urls.regular,
             })) || [];
 
-          setLoading(false);
-
           setImages(photos);
+          setLoading(false);
         })
         .catch(() => {
           console.log("something went wrong!");
         });
     }
-  }, [selectedCategory]);
+  }, [selectedCategory, page]);
 
   useEffect(() => {
     const sortedImages = [...images].sort((a, b) => {
@@ -111,10 +148,8 @@ export default function Home() {
     setImages(sortedImages);
   }, [likesSort]);
 
-  console.log("images", images);
-
   const handleChangeCategory = (event: SelectChangeEvent<string>) => {
-    setSelectedCategory(event.target.value);
+    router.push(`/home?page=1&category=${event.target.value}`);
   };
 
   const handleDateSort = (event: SelectChangeEvent<ESortDirection>) => {
@@ -125,8 +160,59 @@ export default function Home() {
     setLikesSort(event.target.value as ESortDirection);
   };
 
+  const handleLikeClick = (itemId: string) => {
+    if (activeUserEmail) {
+      const userFavorites = localStorage.getItem(activeUserEmail);
+      const userFavoritesParsed = userFavorites
+        ? JSON.parse(userFavorites)
+        : [];
+
+      const isFavoritesExist = userFavoritesParsed.some(
+        (item: string) => item === itemId
+      );
+
+      if (isFavoritesExist) {
+        const filteredFavorites = userFavoritesParsed.filter(
+          (item: string) => item !== itemId
+        );
+        localStorage.setItem(
+          activeUserEmail,
+          JSON.stringify(filteredFavorites)
+        );
+      } else {
+        userFavoritesParsed.push(itemId);
+        localStorage.setItem(
+          activeUserEmail,
+          JSON.stringify(userFavoritesParsed)
+        );
+      }
+      const isFavoritesExistInState = likes.some((item) => item === itemId);
+      if (isFavoritesExistInState) {
+        const isFavoritesExistInStateFiltered = likes.filter(
+          (item: string) => item !== itemId
+        );
+        console.log(
+          "isFavoritesExistInStateFiltered",
+          isFavoritesExistInStateFiltered
+        );
+
+        setLikes(isFavoritesExistInStateFiltered);
+      } else {
+        // setLikes([...likes].push(String(itemId)));
+
+        const likesCopy = [...likes];
+        console.log("likesCopy", likesCopy);
+        likesCopy.push(itemId);
+        console.log("likesCopy2", likesCopy);
+        console.log("itemId2", itemId);
+      }
+    }
+  };
+
+  console.log("likes", likes);
+
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="Generated by create next app" />
@@ -134,26 +220,51 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.header}>
-          <FormControl className={styles.form} fullWidth>
-            <InputLabel id="category-select-label">Category</InputLabel>
+        <header className={styles.header}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              router.push(`/favorites`);
+            }}
+          >
+            Favorites
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              localStorage.setItem("authorized", JSON.stringify(false));
+              localStorage.removeItem("user");
+              router.push(`/`);
+            }}
+          >
+            Log Out
+          </Button>
+        </header>
+
+        <section className={styles.filters}>
+          <div className={styles.selectWrapper}>
+            <InputLabel id="category-select-label">Category:</InputLabel>
             <Select
+              size="small"
               labelId="category-select-label"
+              className={styles.form}
               id="category-select"
               value={selectedCategory}
-              label="Category"
               onChange={handleChangeCategory}
             >
-              <MenuItem value="">All</MenuItem>
+              <MenuItem value="all">All</MenuItem>
               <MenuItem value="864008">Sea</MenuItem>
               <MenuItem value="401930">Mountains</MenuItem>
               <MenuItem value="254504">Desert</MenuItem>
             </Select>
-          </FormControl>
-          <div>
+          </div>
+
+          <div className={styles.sorters}>
             <div className={styles.selectWrapper}>
-              <span className={styles.selectTitle}>Sort by date:</span>
+              <InputLabel id="date-sort-select-label">Sort by date:</InputLabel>
               <Select
+                labelId="date-sort-select-label"
+                size="small"
                 className={styles.form}
                 id="date-sort"
                 value={dateSort}
@@ -164,8 +275,12 @@ export default function Home() {
               </Select>
             </div>
             <div className={styles.selectWrapper}>
-              <span className={styles.selectTitle}>Sort by likes:</span>
+              <InputLabel id="likes-sort-select-label">
+                Sort by likes:
+              </InputLabel>
               <Select
+                size="small"
+                labelId="likes-sort-select-label"
                 className={styles.form}
                 id="likes-sort"
                 value={likesSort}
@@ -176,30 +291,59 @@ export default function Home() {
               </Select>
             </div>
           </div>
-        </div>
+        </section>
 
-        {isLoading ? (
-          <div className={styles.spinnerWrapper}>
-            <CircularProgress className={styles.spinner} />
-          </div>
-        ) : (
-          <div className={styles.imgWrapper}>
-            {images?.map((item) => (
-              <div key={item.id} className={styles.imgContainer}>
-                <img
-                  className={styles.img}
-                  src={item?.url}
-                  alt={item?.alt_description}
-                />
-                <span className={styles.imgDate}>
+        <section className={styles.gallery}>
+          {images?.map((item) => (
+            <div key={item.id} className={styles.imgContainer}>
+              <StarOutlinedIcon
+                onClick={() => handleLikeClick(item.id)}
+                className={
+                  likes?.some((likeItem) => likeItem === item.id)
+                    ? styles.imgLikeActive
+                    : styles.imgLike
+                }
+              />
+              <img
+                className={styles.img}
+                src={item?.url}
+                alt={item?.alt_description}
+              />
+              <div className={styles.info}>
+                <span className={styles.infoText}>
                   {new Date(item.created_at).toLocaleString()}
                 </span>
-                <span className={styles.imgDate}> Likes:{item.likes}</span>
+                <span className={styles.infoText}> Likes:{item.likes}</span>
               </div>
-            ))}
-          </div>
-        )}
+            </div>
+          ))}
+          {isLoading && (
+            <div className={styles.spinnerContainer}>
+              <CircularProgress className={styles.spinner} />
+            </div>
+          )}
+        </section>
+
+        <section className={styles.pagination}>
+          <Button
+            variant="contained"
+            disabled={page === 1}
+            onClick={() =>
+              router.push(`/home?page=${page - 1}&category=${selectedCategory}`)
+            }
+          >
+            Previous
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() =>
+              router.push(`/home?page=${page + 1}&category=${selectedCategory}`)
+            }
+          >
+            Next
+          </Button>
+        </section>
       </main>
-    </>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,63 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import styles from "./Login.module.scss";
 import Button from "@mui/material/Button";
-import data from "../data.json";
-import { createApi } from "unsplash-js";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import { SelectChangeEvent } from "@mui/material/Select";
-import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const userData = [
+  {
+    email: "test@test.test",
+    password: "123test123",
+  },
+  {
+    email: "test1@test.test",
+    password: "321test321",
+  },
+];
+
 export default function Login() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleClick = () => {
+    const emailInput = document.getElementById(
+      "email-input"
+    ) as HTMLInputElement;
+    const passwordInput = document.getElementById(
+      "password-input"
+    ) as HTMLInputElement;
+
+    if (emailInput && passwordInput) {
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      if (email === "" ?? password === "") {
+        setError("Fields should not be empty");
+      } else {
+        const user = userData.find((userData) => userData.email === email);
+
+        if (user) {
+          if (user.password === password) {
+            localStorage.setItem("authorized", "true");
+            localStorage.setItem("user", user.email);
+            router.push("/home");
+          } else {
+            setError("Invalid credentials");
+          }
+        } else {
+          setError("Invalid credentials");
+        }
+      }
+    }
+  };
+
+  const handleInputChange = () => {
+    setError("");
+  };
 
   return (
     <>
@@ -23,9 +66,46 @@ export default function Login() {
         <meta name="description" content="Log in" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        // TODO: Add favicon
+        {/* //TODO: Add favicon */}
       </Head>
-      <main className={`${styles.main} ${inter.className}`}></main>
+      <main className={`${styles.login} ${inter.className}`}>
+        <div className={styles.loginWrapper}>
+          <h1 className={styles.loginTitle}>Log In</h1>
+          <div className={styles.inputs}>
+            <TextField
+              className={styles.input}
+              id="email-input"
+              label="Email"
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+            <TextField
+              className={styles.input}
+              id="password-input"
+              label="Password"
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+          </div>
+          <Button
+            onClick={handleClick}
+            className={styles.button}
+            variant="contained"
+          >
+            Log in
+          </Button>
+
+          {error && <div className={styles.error}>{error}</div>}
+
+          <div className={styles.description}>
+            <div className={styles.descriptionTitle}>
+              To log in, use the following data:
+            </div>
+            <div>1 user - Email: test@test.test, Password: 123test123</div>
+            <div>2 user - Email: test1@test.test, Password: 321test321</div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
